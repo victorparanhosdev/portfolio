4ca0be466184db1787e22ece32dc7621c3662ec2
Victor Paranhos <victor_paranhos@hotmail.com>
2023-03-01 15:38:33 -0300

    atualização

--- js/main.js
+++ js/main.js
@@ -7,37 +7,39 @@ const sr = ScrollReveal({
   distance: '5rem',
   reset: false,
   interval: 100
-
-});
+})
 
 sr.reveal('header', {
-  origin: 'left',
-});
+  origin: 'left'
+})
 
-sr.reveal('div.txt-wrapper-1, .paragraph-1, div.box-model-1, .btn-githuber, .btn-linkedin', {
-  origin: 'right',
-  distance: '10rem',
+sr.reveal(
+  'div.txt-wrapper-1, .paragraph-1, div.box-model-1, .btn-githuber, .btn-linkedin',
+  {
+    origin: 'right',
+    distance: '10rem'
+  }
+)
 
-});
+sr.reveal(
+  '#about, .title-h2, .layout-wrapper-3 article, .wrapper-4 > div, .wrapper-5 > i',
+  {
+    origin: 'bottom',
+    distance: '5rem'
+  }
+)
 
-
-sr.reveal('#about, .title-h2, .layout-wrapper-3 > div, .wrapper-4 > div, .wrapper-5 > i', {
-  origin: 'bottom',
-  distance: '5rem'
-});
-
-
-const menuMobile = document.querySelectorAll(".icon-menu-mobile")
-const listMenu = document.querySelectorAll(".btn-menus li a")
-const header = document.querySelector("header")
+const menuMobile = document.querySelectorAll('.icon-menu-mobile')
+const listMenu = document.querySelectorAll('.btn-menus li a')
+const header = document.querySelector('header')
 const heightHeader = header.offsetHeight
-const botaoParanhos = document.querySelector(".logo")
-const divMenu = document.querySelector(".box-menu")
+const botaoParanhos = document.querySelector('.logo')
+const divMenu = document.querySelector('.box-menu')
 
 for (let ListMenu of listMenu) {
   ListMenu.onclick = () => {
     const isTopFixed = header.style.position == 'fixed'
-    divMenu.classList.remove("active")
+    divMenu.classList.remove('active')
     document.body.style.overflow = 'initial'
     if (isTopFixed) {
       let isClassHidden = document.body.style.overflow == 'hidden'
@@ -45,63 +47,45 @@ for (let ListMenu of listMenu) {
         document.body.style.overflow = 'initial'
       } else {
         document.body.style.overflow = 'hidden'
-
       }
-
     }
   }
 }
 
-
-botaoParanhos.addEventListener("click", () => {
+botaoParanhos.addEventListener('click', () => {
   document.body.style.overflow = 'initial'
-  divMenu.classList.remove("active")
+  divMenu.classList.remove('active')
 })
-
 
 for (let MenuMobile of menuMobile) {
-
   MenuMobile.onclick = () => {
-    divMenu.classList.toggle("active")
+    divMenu.classList.toggle('active')
     let isClassHidden = document.body.style.overflow == 'hidden'
-    
+
     if (isClassHidden) {
       document.body.style.overflow = 'initial'
-
     } else {
       document.body.style.overflow = 'hidden'
-
     }
-
   }
-
-
-
 }
 
-
-
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY > heightHeader) {
-    header.classList.add("scroll")
+    header.classList.add('scroll')
   } else {
-    header.classList.remove("scroll")
+    header.classList.remove('scroll')
   }
- 
 })
 
-
-window.onresize = ()=> {
+window.onresize = () => {
   const widthScreen = window.innerWidth
   let screenWidth = screen.width
   if (widthScreen > 690) {
     let isClassHidden = document.body.style.overflow == 'hidden'
     if (isClassHidden) {
-    document.body.style.overflow = 'initial';
-    divMenu.classList.remove("active");
+      document.body.style.overflow = 'initial'
+      divMenu.classList.remove('active')
+    }
   }
 }
-
-
-};
-
